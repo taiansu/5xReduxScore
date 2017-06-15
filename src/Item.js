777cd17c0d189cccac8bcd1c0f42a9a41a8493f1
Tai An Su <taiansu@gmail.com>
2017-06-16 02:32:02 +0800

    直接在內部元件中綁定事件
    在內部的 Item 元件中，引用 AppActions 中的 tally 函式。接著用不帶參數的 connect() 來包裹 Item 元件，
    這樣一來 Item 元件內就可以用 this.props.dispatch 來對 store 發送事件，而參數就是用 tally 做成的 action 物件。
    
    至此每個語言旁的 [Vote] 按鈕已經可以順利動作了。

--- src/Item.js
+++ src/Item.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
+import {connect} from 'react-redux';
+import {tally} from './AppActions';
 
 class Item extends Component {
+  handleClick(){
+    this.props.dispatch(tally(this.props.lang.name))
+  }
   render(){
     return (
       <div>
         <span>
           {this.props.lang.name}
-          <button>Vote</button>
+          <button onClick={this.handleClick.bind(this)}>Vote</button>
         </span>
       </div>
     )
   }
 }
+
+Item = connect()(Item)
 
 export default Item
